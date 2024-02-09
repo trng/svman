5dcfecb36fb7f24292e32511e211453580f6356f
trng <serhy@ukr.net>
2024-02-10 00:28:46 +0200

    refactoring

--- srt-to-somewhere.sh
+++ srt-to-somewhere.sh
@@ -6,75 +6,27 @@ configs_dir="${this_dir}/srt-to-somewhere-configs"
 YEL='\033[1;33m' # Yellow
 CYA='\033[1;36m' # Cyan
 NC='\033[0m'     # No Color
+backtitle_global='Wrapper for ffmpeg srt inputs v.0.1.1'
 
 
-trap CtrlCandCtrlBreak SIGINT
-CtrlCandCtrlBreak() {
-   exit
-}
+trap trap_ctrl_c SIGINT SIGTERM
+trap_ctrl_c() { exit 1 }
 
-execute_ffmpeg() {
-  trap CtrlCandCtrlBreak SIGINT
-  CtrlCandCtrlBreak() {
-     exit
-  }
 
+execute_config() {
+  trap trap_ctrl_C_in_func SIGINT SIGTERM
+  trap_ctrl_C_in_func() { ctrl_C_pressed=1  }
   while true; do
-    #    FFREPORT=file=/run/ffmpeg-cmd-line-parsed.txt:level=-8 ffmpeg ${ffmpeg_cmd_line}
-    FFREPORT=file=/run/${config_description}.ffmpegpid:level=-8 \
-    ffmpeg ${string_in} ${string_transform} ${string_out}
+    echo ${string_in} ${string_transform} ${string_out}
+    `${string_in} ${string_transform} ${string_out}`
+    [[ ${ctrl_C_pressed} ]] && break
+    sleep 1
   done
 }
-export -f execute_ffmpeg
+export -f execute_config
 
 
-
-
-
-
-
-echo "###########################################################"
-echo "#  Wrapper for ffmpeg srt inputs                          #"
-echo "#  v.0.0.1                                                #"
-echo "###########################################################"
-
-
-while true; do
-
-  # Garbage clean
-  for f in ${temp_dir}/*.ffmpegpid.* ; do
-    screen_pid=${f##*.}
-    screen_name=`basename "${f%.ffmpegpid*}"`
-    linecount=`screen -ls "${screen_name}" | wc -l`
-    if [[ ${linecount} -le 2 ]] ; then
-      # not runnig
-      rm $f
-    fi
-  done
-
-  # Build file list
-  i=0
-  for f in ${configs_dir}/*.conf ; do
-    tmpfname=`basename "$f" .conf` #  ${f%.*}
-    files[i]="$f"
-    linecount=`screen -ls "${tmpfname}" | wc -l`
-    if [[ ${linecount} -le 2  ]] ; then
-      files[i+1]="( NOT RUNING )  ${tmpfname}"
-    else
-      files[i+1]="(   RUNING   )  ${tmpfname}"
-    fi
-    (( i+=2  ))
-  done
-
-
-  # Show dialogue
-  whiptail_result=$(whiptail --title "Configs availible / running" --backtitle "Wrapper for ffmpeg srt inputs v.0.0.1"  --notags --menu "Please select config" 26 98 7 "${files[@]}" 3>&2 2>&1 1>&3- )
-
-  if [[ ! ${whiptail_result}  ]] ; then
-    echo -e "\n${YEL}File not selected. Exiting...${NC}\n"
-    exit
-  fi
-
+config_processing() {
   set -a
   . "${whiptail_result}"
   config_description=`basename "${whiptail_result}" .conf`
@@ -84,64 +36,91 @@ while true; do
       echo "'vars_needed' empty or not set. Cannot continiue. Exiting..."
       exit
   fi
-  echo -e "\n${CYA}CONFIG LOADED:${NC}\n"
-  printf '%-30s = %s\n' "Config file name" "${whiptail_result}"
-
   for var_name in ${vars_needed}; do
     if [[ ! ${!var_name} ]]; then
       echo "'$var_name' empty or not set. Cannot continue."
       exiting=true
     fi
-    printf '%-30s = %s\n' "${var_name}" "${!var_name}"
+    # printf '%-30s = %s\n' "${var_name}" "${!var_name}"
   done
   if [[ $exiting ]] ; then echo "Exiting..." ; exit 1 ; fi
-
-  # Check port in use
-  port_in_use=`ss -Hlnp src ${srt_in_ip} sport ${srt_in_port}`
-  if [[ ${port_in_use} ]]; then
-      echo -e "\n${YEL}WARNING!!! Port '${srt_self_port}' in use!!!${NC}\n"
-  fi
-
-
-  config_in_one_string="ffmpeg ${string_in} ${string_transform} ${string_out}"
 
   # Check script with this description already running
   linecount=`screen -ls "${config_description}" | wc -l`
   if [[ ${linecount} -gt 2 ]] ; then
-    # File with this description in use by screen session
-    ffmpeg_pid=$(screen -S "${config_description}" -Q echo '$PID')
-    ffmpeg_parsed_line_file=/run/${config_description}.ffmpegpid.${ffmpeg_pid}
-    ffmpeg_str_inuse=`grep ffmpeg ${ffmpeg_parsed_line_file}`
-    choices=( 1 "Dive into screen session" )
-    whiptail_result=$(whiptail --title "'${config_description}'" --backtitle "${ffmpeg_parsed_line}" --notags --menu "\n\nIN USE:\n\n\n${ffmpeg_str_inuse}\n\n\n" 20 98 3 "${choices[@]}" 3>&2 2>&1 1>&3- )
-    if [[ ${whiptail_result}  ]] ; then
-	screen -x "${config_description}"
-    fi
+      #
+      # File with this description in use by screen session
+      #
+      pid_of_screen=$(screen -S "${config_description}" -Q echo '$PID')
+      ffmpeg_str_inuse=`ps --ppid ${pid_of_screen} -o pid  --no-headers | xargs ps -o args  --no-headers --ppid`
+      choices=( 1 "Dive into screen session" )
+      whiptail_result=$(whiptail --title "'${config_description}'" --backtitle "${backtitle_global}" --notags --menu "\n\nIN USE - command line from 'ps' output:\n\n\n${ffmpeg_str_inuse}\n\n\n" 20 98 3 "${choices[@]}" 3>&2 2>&1 1>&3- )
+      if [[ ${whiptail_result}  ]] ; then
+          screen -x ${pid_of_screen} # "${config_description}"
+      fi
   else
-    # File with this description not in use by screen session
-    choices=( 1 "Wrap script to screen session" 2 "Run script here" )
-    whiptail_result=$(whiptail --title "'${config_description}'" --backtitle "${ffmpeg_parsed_line}" --notags --menu "\n\n\n${config_in_one_string}\n\n\n" 20 98 3 "${choices[@]}" 3>&2 2>&1 1>&3- )
-    if [[ ${whiptail_result}  ]] ; then
-        if [[ ${whiptail_result} -eq 1  ]] ; then
-            #screen -d -m -S "${config_description}" "${this_dir}/srt-to-somewhere-exec.sh"
-    	    screen -d -m -S "${config_description}" bash -c "execute_ffmpeg"
-	    ffmpeg_pid=$(screen -S "${config_description}" -Q echo '$PID')
-	    if [ $? -eq 0 ] ; then
-		sleep 2
-		ffmpeg_parsed_line_file=/run/${config_description}.ffmpegpid
-		mv ${ffmpeg_parsed_line_file} ${ffmpeg_parsed_line_file}.${ffmpeg_pid}
-		ffmpeg_parsed_line_file=${ffmpeg_parsed_line_file}.${ffmpeg_pid}
-		echo -e "\n${CYA}\nffmpeg parsed line:\n\n\n"
-		grep ffmpeg ${ffmpeg_parsed_line_file}
-		echo -e "\n${NC}\n\n\n"
-		sleep 3
-	    fi
+      #
+      # File with this description not in use by any screen session
+      #
 
-        else
-            #("${this_dir}/srt-to-somewhere-exec.sh") #`${script_temp_name}`
-    	    execute_ffmpeg
-	fi
-    fi
+      # Check port in use (remote host іs also checked but this isn't problem - ss just retrun nothing)
+      [[ ${srt_in_ip} ]] && [[ ${srt_in_port} ]] && port_in_use=`ss -Hlnp src ${srt_in_ip} sport ${srt_in_port}`
+      warning_str='\n\nSELECTED CONFIGURATION:\n\n'
+      [[ ${port_in_use} ]] && warning_str="\n\n  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n  !!   WARNING!!!!  Port $( printf %-7s "'${srt_in_port}'") in use!   !!!!\n  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n\n"
+
+      choices=( 1 "Wrap script to screen session" 2 "Run script here" )
+      whiptail_result=$(whiptail --title "'${config_description}'" --backtitle "${backtitle_global}" --notags --menu "${warning_str} ${string_in} ${string_transform} ${string_out}\n\n\n\n\n" 22 98 3 "${choices[@]}" 3>&2 2>&1 1>&3- )
+      if [[ ${whiptail_result} ]] ; then
+          [[ ${whiptail_result} -eq 1 ]] && screen -d -m -S "${config_description}" bash -c "execute_config" || execute_config
+      fi
   fi
+
+} # end of config_processing()
+
+
+
+
+######################################################################
+#                                                                    #
+# Main loop                                                          #
+#                                                                    #
+######################################################################
+
+while true; do
+
+    # Build config files list
+    i=0
+    active_srt_in_ports=()
+    #for f in ${configs_dir}/*.conf ; do
+    for f in `LC_ALL=C ls --format='single-column' ${configs_dir}/*.conf`; do
+      tmpfname=`basename "$f" .conf` #  ${f%.*}
+      files[i]="$f"
+      linecount=`screen -ls "${tmpfname}" | wc -l`
+      if [[ ${linecount} -le 2  ]] ; then
+          files[i+1]="( NOT RUNNING )  ${tmpfname}"
+      else
+          files[i+1]="(   RUNNING   )  ${tmpfname}"
+          set -a
+          . "${f}"
+          set +a
+          active_srt_in_ports+=( $srt_in_port  )
+      fi
+      (( i+=2  ))
+    done
+
+    # list of used srt listen ports (selected config will be checked with this list)
+    str=${active_srt_in_ports[*]}
+    str='port '${str// / or port\ }
+    [[ ${#active_srt_in_ports[*]} -eq 1 ]] && active_srt_in_ports=${str} || active_srt_in_ports='( '$str' )'
+
+    # Show dialogue
+    export NEWT_COLORS='
+      root=,blue
+      textbox=blue,
+    '
+    whiptail_result=$(whiptail --title "Configs availible / running" --backtitle "${backtitle_global}"  --notags --menu "Please select config" 26 98 12 "${files[@]}" 3>&2 2>&1 1>&3- )
+    if [[ ! ${whiptail_result}  ]] ; then echo -e "\n${YEL}Config not selected. Exiting...${NC}\n"; exit ; fi
+
+    config_processing
 
 done
